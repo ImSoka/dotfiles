eaae5e4750b69cdd4cb967b5209c8ff39f905b3e
Victor Araque Casaus <victor@fdos.net>
2021-08-03 21:14:22 +0200

    add fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,17 @@
+#   _____       _
+#  / ____|     | |               .--. _ .-.
+# | (___   ___ | | ____ _       : .-':_;: :
+#  \___ \ / _ \| |/ / _` |      : `; .-.: :   .--.  .--.
+#  ____) | (_) |   < (_| |    _ : :  : :: :_ ' '_.'`._-.'
+# |_____/ \___/|_|\_\__,_|   :_;:_;  :_;`.__;`.__.'`.__.' 
+#
+#  Fish shell configuration
+
+# Aliases
+alias ls 'ls -l --color=auto'
+alias ls 'ls -lA'
+
+
+#Git
+
+alias dotfiles 'git --git-dir=$HOME/dotfiles --work-tree=$HOME'
